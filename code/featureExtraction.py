427e2065030e10037bd2fde84d2cec3b269a7757
sachinmalepati <sachin.malepati@gmail.com>
2017-12-03 17:07:54 +0530

    generating and storing features into a file

--- code/featureExtraction.py
+++ code/featureExtraction.py
@@ -14,8 +14,28 @@ import csv
 from glob import glob
 import pandas as pd
 import matplotlib.pyplot as plt
-
+import math
 from PIL import Image
+
+from scipy import ndimage as ndi
+
+from skimage.morphology import watershed
+from skimage.feature import peak_local_max
+from skimage.segmentation import random_walker
+from sklearn.cluster import spectral_clustering
+from sklearn.feature_extraction import image
+
+from skimage import restoration
+from skimage import img_as_float
+from skimage.filters import roberts, sobel, scharr, prewitt
+from skimage.morphology import skeletonize,skeletonize_3d,medial_axis,thin
+
+from math import sqrt
+from skimage import data
+from skimage.feature import blob_dog, blob_log, blob_doh
+from skimage.color import rgb2gray
+
+from skimage.morphology import convex_hull_image
 
 def load_image( infilename ) :
     img = Image.open( infilename )
@@ -29,54 +49,133 @@ def normalising0to1(img):
     img=img/range1
     return img
 
-def getRegionMetricRow(fname = "../data/subset0_candidates/subset0_positive_93273_134671.jpg"):
-    seg = load_image(fname)
-    seg = normalising0to1(seg)
-    plt.title("Image after normalising0to1")
-    plt.imshow(seg,cmap="gray")
-        
-    plt.show()
-    #metrics
-    totalArea = 0.
-    avgArea = 0.
-    maxArea = 0.
-    avgEcc = 0.
-    avgEquivlentDiameter = 0.
-    stdEquivlentDiameter = 0.
-    weightedX = 0.
-    weightedY = 0.
-    numNodes = 0.
-    numNodesperSlice = 0.
-    # do not allow any nodes to be larger than 10% of the pixels to eliminate background regions
-    maxAllowedArea = 0.10 * 512 * 512 
-    areas = []
-    eqDiameters = []
-    regions = getRegionFromMap(seg)
-    for region in regions:
-        if region.area > maxAllowedArea:
-            continue
-        totalArea += region.area
-        areas.append(region.area)
-        avgEcc += region.eccentricity
-        avgEquivlentDiameter += region.equivalent_diameter
-        eqDiameters.append(region.equivalent_diameter)
-        weightedX += region.centroid[0]*region.area
-        weightedY += region.centroid[1]*region.area
-        numNodes += 1
-    weightedX = weightedX / totalArea 
-    weightedY = weightedY / totalArea
-    avgArea = totalArea / numNodes
-    avgEcc = avgEcc / numNodes
-    avgEquivlentDiameter = avgEquivlentDiameter / numNodes
-    stdEquivlentDiameter = np.std(eqDiameters)
-    maxArea = max(areas)
-    return np.array([avgArea,maxArea,avgEcc,avgEquivlentDiameter, 
-        stdEquivlentDiameter, weightedX, weightedY, numNodes])
+def medianFilter(img):
+	 img=ndimage.median_filter(img,3)
+	 return img
+
+def getRegionMetricRow(fname = "../data/subset0_candidates/subset0_positive_12111_107806.jpg"):
+	seg = load_image(fname)
+	seg = normalising0to1(seg)
+	# plt.title("Image after normalising0to1")
+	# plt.imshow(seg,cmap="gray")    
+	# plt.show()
+
+	#metrics
+	totalArea = 0.0
+	Ecc = 0.0
+	EquivlentDiameter = 0.0
+	weightedX = 0.0
+	weightedY = 0.0
+	numNodes = 0.0
+	Rectangularity = 0.0
+	MeanIntensity = 0.0
+	Perimeter = 0.0
+	Circularity = 0.0
+	Elongation = 0.0
+	EulerNumber = 0.0
+
+	f1=fname.split("/")
+	fileName="".join(f1[3])
+	f2=f1[3].split("_")
+	classLabel=f2[1]
+	if classLabel == "positive":
+		classLabel=1
+	elif classLabel == "negative":
+		classLabel=0
+
+	original = seg
+	regions, labels, numberOfRegions = getRegionFromMap(seg)
+
+	required_label = 0
+	for i in range(30,35):
+		for j in range(30,35):
+			if labels[i,j]!=0:
+				required_label = labels[i,j]
+				break
+
+	#print("label: "+str(required_label))
+	if required_label == 0 :
+		print("No nodules found")
+		return ([fileName, totalArea,Ecc,EquivlentDiameter, weightedX, weightedY, Rectangularity, 
+			MeanIntensity, Circularity, Elongation, EulerNumber, classLabel])
+
+	nodule_region = 0
+
+	for region in regions:
+		B = region.bbox
+		if region.label == required_label:
+			print(str(B[0])+" "+str(B[1])+" "+str(B[2])+" "+str(B[3])+" ")
+			#region should not have the area of 64x64 
+			if region.area>= 0.7*64*64:
+				break
+			
+			if B[0] == 0 or B[1] == 0 or B[2] == 64 or B[3]==64:
+
+				#if B[0] == 0 and B[1] == 0 and B[2] == 64 and B[3]==64:
+					#break				
+				mask = np.where(labels == region.label, 1, 0)
+				segmented_region = original * mask
+				segmented_region_bw = np.where(segmented_region==0,1,0)
+				km=morphology.erosion(segmented_region_bw,np.ones([3,3]))
+				#img_fill_holes = ndimage.binary_fill_holes(km)
+				chull=convex_hull_image(km)
+				chull=morphology.erosion(chull,np.ones([3,3]))
+
+				seg_nodule = segmented_region * chull
+				seg_nodule = morphology.dilation(seg_nodule, np.ones([2,2]))
+				# plt.title("segmented nodule")
+				# plt.imshow(seg_nodule,cmap="gray")
+				# plt.show()
+
+				threshold = 0.44
+
+				labeled, nr_objects = ndimage.label(seg_nodule > threshold)
+				new_regions = measure.regionprops(labeled)
+
+				if nr_objects == 0:
+					break
+				max_area=0
+				for new_region in new_regions:
+					if new_region.area >= max_area:
+						nodule_region = new_region
+				break
+
+			nodule_region = region
+			newB = nodule_region.bbox
+			# if newB[0] == 0 or newB[1] == 0 or newB[2] == 64 or newB[3]==64:
+			# 	break
+			break
+
+		else:
+			continue
+
+	if nodule_region == 0:
+		print("No nodules found")
+		return ([fileName, totalArea,Ecc,EquivlentDiameter, weightedX, weightedY, Rectangularity, 
+			MeanIntensity, Circularity, Elongation, EulerNumber, classLabel])
+	region = nodule_region
+	B = region.bbox
+	totalArea = region.area
+	Perimeter = region.perimeter
+	Ecc = region.eccentricity
+	EquivlentDiameter = region.equivalent_diameter
+	weightedX += region.centroid[0]
+	weightedY += region.centroid[1]
+	numNodes += 1
+	Rectangularity = region.extent
+	#MeanIntensity = region.mean_intensity
+	Circularity = (Perimeter*Perimeter)/(4*math.pi*totalArea)
+	width = B[3] - B[1]
+	height = B[2] - B[0]
+	Elongation = min(height,width) / max(height, width)
+	EulerNumber = region.euler_number
+	return ([fileName, totalArea,Ecc,EquivlentDiameter, weightedX, weightedY, Rectangularity, 
+		MeanIntensity, Circularity, Elongation, EulerNumber, classLabel])
 
 
 
 def getRegionFromMap(slice_npy):
-    print("mean",np.mean(slice_npy))
+    #print("mean",np.mean(slice_npy))
     #thr = np.where(slice_npy > np.mean(slice_npy),0.,1.0)
 
     #plt.title("Ihresholding")
@@ -87,24 +186,49 @@ def getRegionFromMap(slice_npy):
     #labels = label_image.astype(int)
     #regions = measure.regionprops(labels)
 
-    blur_radius = 1.0
-    threshold = 0.5
+    blur_radius = 0.5
+    threshold = 0.44
+
+    # eq=equalize_hist(slice_npy)
+    # plt.subplot(4,3,4)
+    # plt.title("Histogram equlised image")
+    # plt.imshow(eq,cmap="gray")
+
+    #eq = np.where(eq>0.44,eq,0)
 
     imgf = ndimage.gaussian_filter(slice_npy, blur_radius)
+    #plt.title("after gaussian_filter")
+    #plt.imshow(imgf,cmap="gray")
+    #plt.show()
 
-    plt.title("after gaussian_filter")
-    plt.imshow(imgf,cmap="gray")
-    plt.show()
+    median = medianFilter(imgf)
+    # plt.title("after median_filter")
+    # plt.imshow(median,cmap="gray")
+    # plt.show()
 
-    labeled, nr_objects = ndimage.label(imgf > threshold)
-    print("no of objects", nr_objects)
+    #test(median)
+
+
+    labeled, nr_objects = ndimage.label(median > threshold)
+    #print("no of objects", nr_objects)
     regions = measure.regionprops(labeled)
 
-    plt.title("regions")
-    plt.imshow(labeled,cmap="gray")
-    plt.show()
+    # plt.title("regions")
+    # plt.imshow(labeled,cmap="gray")
+    # plt.show()
 
-    return regions
+    return regions, labeled, nr_objects
+
+def OTSUsegmentation(img):
+	thresh=filters.threshold_otsu(img)
+	otsu=np.where(img<0.44,0,1)
+	#otsu=morphology.erosion(otsu,np.ones([3,3]))
+	otsu=morphology.erosion(otsu,np.ones([3,3]))
+	#otsu=np.where(otsu==1,0,1)
+	return otsu
+
+
+
 
 
 features=getRegionMetricRow()
