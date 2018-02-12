8565902e4c55a8a3caeb0ed62272723cd1814911
mohith22 <mohithdamarpati@gmail.com>
2018-02-12 17:41:22 +0530

    New datasets made:

--- code/makeData.py
+++ code/makeData.py
@@ -9,19 +9,22 @@ import pandas as pd #Pandas for manipulating data frames
 import numpy as np #Numpy for operations on n-dimensional arrays
 from shutil import copyfile #For copying the image files from one location to other
 from PIL import Image
+import os
 #Data frame is loaded from the features csv which is generated previously
 df = pd.read_csv('../data/allSubsetsFeatures.csv')
 
 print df['Class']
 
-ratio = 0.2
+ratio = 0.5
 print(ratio)
 
-total_neg=1360*0.25
 total_pos=1360
+total=int(total_pos/(1-ratio))
+total_neg=int(total * ratio)
+
 
 df_neg = df[df['Class']==0]
-df_neg = df_neg.sample(n=total_neg)
+df_neg = df_neg.sample(n=int(total_neg))
 df_pos = df[df['Class']==1]
 
 print(len(df_pos))
@@ -33,7 +36,7 @@ df_pos = df_pos.sample(frac=1)
 
 #Train and Test Set - Negatives
 
-split = 0.8
+split = 0.6
 
 df_neg_train = df_neg[0:int(total_neg*split)]
 df_neg_test = df_neg[int(total_neg*split):total_neg]
@@ -53,21 +56,33 @@ df_fin_test = df_fin_test.sample(frac=1)
 
 
 #Randomly chosen data is transfered from the original dataset to a new folder which is then used in training the CNN model
+directory_train='Train_' + str(split)+'_Neg_'+str(ratio)+'_Data/'
+if not os.path.exists('../data/DataSets/'+directory_train):
+	os.makedirs('../data/DataSets/'+directory_train)
+
 for i in df_fin_train['file_name']:
 	n = i[6]
 	im = Image.open('../../../Data_Nodet/data/data/subset'+str(n)+'_candidates/'+i)
 	width, height = im.size
 	if width==64 and height==64:
+		
 		copyfile('../../../Data_Nodet/data/data/subset'+str(n)+'_candidates/'+i,'../data/DataSets/Train_' + str(split)+'_Neg_'+str(ratio)+'_Data/' + i)
 		print i
 	else:
 		df_fin_train = df_fin_train[df_fin_train.file_name != i]
 
+test_directory='Test_' + str(split)+'_Neg_'+str(ratio)+'_Data/'
+
+if not os.path.exists('../data/DataSets/'+test_directory):
+	os.makedirs('../data/DataSets/'+test_directory)
+
 for i in df_fin_test['file_name']:
 	n = i[6]
 	im = Image.open('../../../Data_Nodet/data/data/subset'+str(n)+'_candidates/'+i)
 	width, height = im.size
+
 	if width==64 and height==64:
+		
 		copyfile('../../../Data_Nodet/data/data/subset'+str(n)+'_candidates/'+i,'../data/DataSets/Test_'+ str(split)+'_Neg_'+str(ratio)+'_Data/' + i)
 		print i
 	else:
