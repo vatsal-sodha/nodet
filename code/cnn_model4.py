11a3eef484e86ed6412512526cebe4e4839827b9
CUDA Master <cuda-master@JHA>
2018-02-13 15:16:52 +0530

    Normal Data Results Done

--- code/cnn_model4.py
+++ code/cnn_model4.py
@@ -90,18 +90,18 @@ def cnn_model_fn(features, labels, mode):
 
 #Loading the Train and Test data that are prepared by me in the data_prep.py
 def load_sampled_data_(j):
-  trainExamples=1810
-  testExamples=454
+  trainExamples=1630
+  testExamples=1088
   if j==0:
     iterations=trainExamples
   else:
     iterations=testExamples
 
   if j==0:
-    file_list = glob('../data/DataSets/Train_0.8_Neg_0.4_Data/*')
+    file_list = glob('../data/DataSets/Train_0.6_Neg_0.5_Data/*')
     train_data = np.zeros((trainExamples,4096))
   if j==1:
-    file_list = glob('../data/DataSets/Test_0.8_Neg_0.4_Data/*')
+    file_list = glob('../data/DataSets/Test_0.6_Neg_0.5_Data/*')
     train_data = np.zeros((testExamples,4096))
 
   i = 0
@@ -160,7 +160,7 @@ def main(unused_argv):
 
 
   # Create the Estimator
-  nodet_classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir="./../../../Models/cnn_model4_PosNeg60:40_TrainTest80:20")
+  nodet_classifier = tf.estimator.Estimator(model_fn=cnn_model_fn, model_dir="./../../../Models/cnn_model4_PosNeg50:50_TrainTest60:40")
 
   # Set up logging for predictions
   tensors_to_log = {"probabilities": "softmax_tensor"}
@@ -175,7 +175,7 @@ def main(unused_argv):
 
   output_str=""
 
-  for steps in range(10):
+  for steps in range(5):
 
     # Classifier
     nodet_classifier.train(input_fn=train_input_fn,steps=100,hooks=[logging_hook])
