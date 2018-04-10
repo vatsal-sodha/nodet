b9ab532cbd9007aba241efdf41856061818d5671
CUDA Master <cuda-master@JHA>
2018-04-10 15:04:29 +0530

    Done preProcessed_1

--- code/makeObjdetData.py
+++ code/makeObjdetData.py
@@ -11,8 +11,10 @@ from shutil import copyfile #For copying the image files from one location to ot
 from PIL import Image
 import os
 
-df = pd.read_csv('../data/nodules.csv')
+df = pd.read_csv('../data/nodules_preProcessed1.csv')
 
+#shuffle
+df.sample(frac=1)
 total = len(df)
 print("Length")
 print(total)
@@ -32,21 +34,21 @@ print(len(df_train))
 print(len(df_test))
 
 #Randomly chosen data is transfered from the original dataset to a new folder which is then used in training the CNN model
-directory_train='../data/ObjdetDataSet/train/'
+directory_train='../data/ObjdetDataSet/train_preProcessed_1/'
 if not os.path.exists(directory_train):
 	os.makedirs(directory_train)
 
 for i in df_train['filename']:
-	im = Image.open('../data/Nodules/'+i)
-	copyfile('../data/Nodules/'+i, directory_train + i)
+	im = Image.open('../data/nodules_preProcessed_1/'+i)
+	copyfile('../data/nodules_preProcessed_1/'+i, directory_train + i)
 
-directory_test='../data/ObjdetDataSet/test/'
+directory_test='../data/ObjdetDataSet/test_preProcessed_1/'
 if not os.path.exists(directory_test):
 	os.makedirs(directory_test)
 
 for i in df_test['filename']:
-	im = Image.open('../data/Nodules/'+i)
-	copyfile('../data/Nodules/'+i, directory_test + i)
+	im = Image.open('../data/nodules_preProcessed_1/'+i)
+	copyfile('../data/nodules_preProcessed_1/'+i, directory_test + i)
 
-df_train.to_csv('../data/ObjdetDataSet/train.csv',index=False)
-df_test.to_csv('../data/ObjdetDataSet/test.csv'	,index=False)
+df_train.to_csv('../data/ObjdetDataSet/train_preProcessed_1.csv',index=False)
+df_test.to_csv('../data/ObjdetDataSet/test_preProcessed_1.csv'	,index=False)
