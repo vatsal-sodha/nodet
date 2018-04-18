29c123c627ad5f3894b78227855641cf4ceae680
CUDA Master <cuda-master@JHA>
2018-04-18 13:10:36 +0530

    Plot Nodules

--- code/plotNodulesArea.py
+++ code/plotNodulesArea.py
@@ -7,10 +7,13 @@ import matplotlib.pyplot as plt
 df = pd.read_csv('../data/nodules_preProcessed2.csv')
 diameter=df['width']
 # print(diameter[0])
+resize_factor=1.5
 diameter *=3.14
+diameter *= resize_factor
 diameter /=4
-# print(diameter[0])
+print(diameter.mean())
 plt.xlabel("Aread of nodules")
 plt.ylabel("Frequency count")
-plt.hist(diameter, bins=np.arange(diameter.min(), diameter.max()+1))
+# plt.hist(diameter, bins=np.arange(diameter.min(), diameter.max()+1))
+plt.hist(diameter,bins=50)
 plt.show()
