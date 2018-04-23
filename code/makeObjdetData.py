e47662d1ba306795807cf879cc72f6b29fd3b259
CUDA Master <cuda-master@JHA>
2018-04-23 13:34:32 +0530

    Done merging subset 6 with csv and images

--- code/makeObjdetData.py
+++ code/makeObjdetData.py
@@ -12,7 +12,7 @@ from PIL import Image
 import os
 from sklearn.utils import shuffle
 from sklearn.cross_validation import train_test_split
-df = pd.read_csv('../data/nodules_preProcessed2.csv')
+df = pd.read_csv('../data/nodules_preProcessed_Lungs_1.csv')
 #shuffle
 df=shuffle(df)
 total = len(df)
@@ -34,21 +34,21 @@ print(len(df_train))
 print(len(df_test))
 
 #Randomly chosen data is transfered from the original dataset to a new folder which is then used in training the CNN model
-directory_train='../data/ObjdetDataSet/train_preProcessed_2/'
+directory_train='../data/ObjdetDataSet/train_preProcessed_3/'
 if not os.path.exists(directory_train):
 	os.makedirs(directory_train)
 
 for i in df_train['filename']:
-	im = Image.open('../data/Lungs_without_resizing/'+i)
-	copyfile('../data/Lungs_without_resizing/'+i, directory_train + i)
+	im = Image.open('../data/Lungs_with_resizing/'+i)
+	copyfile('../data/Lungs_with_resizing/'+i, directory_train + i)
 
-directory_test='../data/ObjdetDataSet/test_preProcessed_2/'
+directory_test='../data/ObjdetDataSet/test_preProcessed_3/'
 if not os.path.exists(directory_test):
 	os.makedirs(directory_test)
 
 for i in df_test['filename']:
-	im = Image.open('../data/Lungs_without_resizing/'+i)
-	copyfile('../data/Lungs_without_resizing/'+i, directory_test + i)
+	im = Image.open('../data/Lungs_with_resizing/'+i)
+	copyfile('../data/Lungs_with_resizing/'+i, directory_test + i)
 
-df_train.to_csv('../data/ObjdetDataSet/train_preProcessed_2.csv',index=False)
-df_test.to_csv('../data/ObjdetDataSet/test_preProcessed_2.csv'	,index=False)
+df_train.to_csv('../data/ObjdetDataSet/train_preProcessed_3.csv',index=False)
+df_test.to_csv('../data/ObjdetDataSet/test_preProcessed_3.csv'	,index=False)
